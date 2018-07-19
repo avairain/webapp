3dd2fc947b0e52050a199154af321eaac15b1831
yaoxingyu <xingyu.yao@cienet.com.cn>
2018-07-19 12:35:16 +0800

    node build test

--- config/index.js
+++ config/index.js
@@ -60,7 +60,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     // assetsPublicPath: '/',
-    assetsPublicPath: '/v1/rtmapp/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
